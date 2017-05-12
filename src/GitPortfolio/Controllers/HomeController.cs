526084c4bed7fe2be9dc0eb304774056fecd6d5f
Melvin Gruschow <melvin.f.gruschow@gmail.com>
2017-05-12 09:25:57 -0700

    setup base files and folders

---
+++ src/GitPortfolio/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GitPortfolio.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GitPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Github()
+        {
+            return View();
+        }
+    }
+}
